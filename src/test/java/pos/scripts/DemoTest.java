c5cef173e46e550cb49cac66a8d462cad1e9f4d9
malok <malok@LAPTOP-1BLM5OS8.mshome.net>
2023-06-26 16:22:46 +0530

    pos project commited

---
+++ src/test/java/pos/scripts/DemoTest.java
@@ -0,0 +1,14 @@
+package pos.scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import generic.BaseTest;
+
+public class DemoTest extends BaseTest {
+@Test
+public void tesA()
+{
+Reporter.log("hi");	
+}
+}
